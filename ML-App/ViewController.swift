161701d88bc73709ec0de6314130c617901e9a06
Sam Sabah <sam.sabah@exxeta.com>
2020-05-21 02:40:21 +0200

    Initial Commit

---
+++ ML-App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ML-App
+//
+//  Created by Sabah, Sam on 21.05.20.
+//  Copyright © 2020 Sabah, Sam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
